647ac6ecd2a9b0cccd763b5df2ee42d3d20a4b6e
chiku792074244 <kumkumjha1080@gmail.com>
2026-04-20 17:23:42 +0530

    Update coupledp.js

--- Priyansh/commands/coupledp.js
+++ Priyansh/commands/coupledp.js
@@ -1,40 +1,56 @@
 const axios = require("axios");
 const fs = require("fs-extra");
+const path = require("path");
 
 module.exports.config = {
- name: "coupledp",
- version: "1.0",
- haspermission: 0,
- credit: "Loid Butter",
- cooldown: 5,
- description: "couple dp",
- commandCategory: "image",
- dependencies: {
-   "axios":""
- }
- };
+  name: "coupledp",
+  version: "1.1",
+  haspermission: 0,
+  credit: "Loid Butter (Fixed by ChatGPT)",
+  cooldown: 5,
+  description: "Get random couple dp",
+  commandCategory: "image"
+};
 
- module.exports.run = async function ({ api, event, args }) {
- try {
- const { data } = await axios.get(
- "https://tanjiro-api.onrender.com/cdp?api_key=tanjiro"
- );
- const maleImg = await axios.get(data.male, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/cache/img1.png", Buffer.from(maleImg.data, "utf-8"));
- const femaleImg = await axios.get(data.female, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/cache/img2.png", Buffer.from(femaleImg.data, "utf-8"));
+module.exports.run = async function ({ api, event }) {
+  try {
+    const cachePath = path.join(__dirname, "cache");
 
- const msg = "Here is your couple dp";
- const allImages = [
- fs.createReadStream(__dirname + "/cache/img1.png"),
- fs.createReadStream(__dirname + "/cache/img2.png")
- ];
- 
- return api.sendMessage({
- body: msg,
- attachment: allImages
- }, event.threadID, event.messageID);
- } catch (error) {
- console.error(error);
- }
+    // ensure cache folder exists
+    if (!fs.existsSync(cachePath)) {
+      fs.mkdirSync(cachePath);
+    }
+
+    // fetch API data
+    const { data } = await axios.get(
+      "https://tanjiro-api.onrender.com/cdp?api_key=tanjiro"
+    );
+
+    // download male image
+    const maleImg = await axios.get(data.male, {
+      responseType: "arraybuffer"
+    });
+    const malePath = path.join(cachePath, "img1.png");
+    fs.writeFileSync(malePath, maleImg.data);
+
+    // download female image
+    const femaleImg = await axios.get(data.female, {
+      responseType: "arraybuffer"
+    });
+    const femalePath = path.join(cachePath, "img2.png");
+    fs.writeFileSync(femalePath, femaleImg.data);
+
+    // send message
+    return api.sendMessage({
+      body: "💑 Here is your Couple DP",
+      attachment: [
+        fs.createReadStream(malePath),
+        fs.createReadStream(femalePath)
+      ]
+    }, event.threadID, event.messageID);
+
+  } catch (error) {
+    console.error(error);
+    return api.sendMessage("❌ Error fetching couple dp!", event.threadID, event.messageID);
+  }
 };
